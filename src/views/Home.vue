98e45d5a4e19de26446b07c98eaabb27f48f9f3e
kanglerian <kanglerian@gmail.com>
2021-01-23 20:54:18 +0700

    update conver eps 5

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HeroBanner/>
+    <WomenBanner/>
+  </div>
+</template>
+
+<script>
+
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroBanner from '@/components/HeroBanner.vue';
+import WomenBanner from '@/components/WomenBanner';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroBanner,
+    WomenBanner
+  }
+}
+</script>
