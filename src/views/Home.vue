c957f61a82468f5f1e8f7ba93d15f6374f9af878
kanglerian <kanglerian@gmail.com>
2021-01-24 10:15:31 +0700

    rest page eps 6

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="home">
-    <HeaderShayna/>
-    <HeroBanner/>
-    <WomenBanner/>
+    <HeaderShayna />
+    <HeroBanner />
+    <WomenBanner />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
   </div>
 </template>
 
 <script>
-
-import HeaderShayna from '@/components/HeaderShayna.vue';
-import HeroBanner from '@/components/HeroBanner.vue';
-import WomenBanner from '@/components/WomenBanner';
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroBanner from "@/components/HeroBanner.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HeaderShayna,
     HeroBanner,
-    WomenBanner
-  }
-}
+    WomenBanner,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
 </script>
